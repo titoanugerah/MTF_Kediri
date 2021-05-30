52aa32d785bee741ecd3cc470a14a10e2d89f65b
titoanugerah <titoanugerah@student.undip.ac.id>
2021-05-31 05:01:27 +0700

    Resolve naming model

--- application/models/HomeCMS_model.php
+++ application/models/HomeCMS_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class HomeCms_model extends CI_Model
+class HomeCMS_model extends CI_Model
 {
 
   function __construct()
--- application/models/TermsCMS_model.php
+++ application/models/TermsCMS_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class TermsCms_model extends CI_Model
+class TermsCMS_model extends CI_Model
 {
 
   function __construct()
