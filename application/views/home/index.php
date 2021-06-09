f5a1bf593ce569b5aab22b8da124ca15c17def3c
titoanugerah <titoanugerah@student.undip.ac.id>
2021-06-09 11:52:36 +0700

    Change color

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -95,63 +95,43 @@
 
     <div class="row">
 
-      <div class="col-lg-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-        <div class="info">
-          <div class="address">
-            <i class="icofont-google-map"></i>
-            <h4>Alamat Kami:</h4>
-            <p><?php echo $contact->address; ?></p>
-          </div>
-
-          <div class="email">
-            <i class="icofont-envelope"></i>
-            <h4>Email</h4>
-            <p><?php echo $contact->email; ?></p>
-          </div>
-
-          <div class="phone">
-            <i class="icofont-phone"></i>
-            <h4>Telepon:</h4>
-            <p><?php echo $contact->phone; ?></p>
-          </div>
-
-          <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe> -->
-        </div>
-
-      </div>
-
-      <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
         <form action="forms/contact.php" method="post" role="form" class="php-email-form">
           <div class="form-row">
             <div class="form-group col-md-6">
-              <label for="name">Your Name</label>
+              <label for="name">Nama</label>
               <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
               <div class="validate"></div>
             </div>
             <div class="form-group col-md-6">
-              <label for="name">Your Email</label>
-              <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
+              <label for="name">No KTP</label>
+              <input type="text" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
               <div class="validate"></div>
             </div>
           </div>
           <div class="form-group">
-            <label for="name">Subject</label>
-            <input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+            <label for="name">NPWP</label>
+`               <input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
             <div class="validate"></div>
           </div>
           <div class="form-group">
-            <label for="name">Message</label>
+            <label for="name">Nomor Rekening</label>
             <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
             <div class="validate"></div>
           </div>
-          <div class="mb-3">
-            <div class="loading">Loading</div>
-            <div class="error-message"></div>
-            <div class="sent-message">Your message has been sent. Thank you!</div>
+          <div class="form-group">
+            <label for="name">Email</label>
+            <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
+            <div class="validate"></div>
           </div>
+          <div class="form-group">
+            <label for="name">Pekerjaan</label>
+            <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
+            <div class="validate"></div>
+          </div>
+
+
           <div class="text-center"><button type="submit">Send Message</button></div>
         </form>
-      </div>
 
     </div>
 
